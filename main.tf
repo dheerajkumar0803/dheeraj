baa94a220f01751120a2897b11f2e558f4679cd9
Dheeraj 1. Kumar (EXT-Nokia) <dheeraj.1.kumar.ext@nokia.com>
2024-11-29 15:03:39 +0530

    added

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
 resource "azurerm_resource_group" "example" {
-  name     = "dheeraj"
+  name     = "dheeraj01"
   location = "west us"
 }
 
-resource "azurerm_resource_group" "file1" {
-  name     = "devops"
-  location = "canada east"
+resource "azurerm_storage_account" "stg" {
+  name = "stg0021"
+  location = "west us"
+  resource_group_name = "dheeraj01"
+  account_tier = "standard"
+  account_replication_type = "LRS"
+  
 }
